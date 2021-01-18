a806d3d41f5e3a53a3130ae573b98ac34de3ba5c
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-01-18 12:43:15 +0100

    Simplify dependencies.sh

--- buildpacks/maven/lib/dependencies.sh
+++ buildpacks/maven/lib/dependencies.sh
@@ -4,26 +4,28 @@ dependencies::has_spring_boot() {
 	local -r app_directory=${1:?}
 	local -r pom_path="${app_directory}/pom.xml"
 
-	[[ -f "${pom_path}" ]] &&
-		[[ -n "$(grep "<groupId>org.springframework.boot" "${pom_path}")" ]] &&
-		[[ -n "$(grep "<artifactId>spring-boot" "${pom_path}")" ]]
+	if [[ -f "${pom_path}" ]]; then
+		grep -q "<groupId>org.springframework.boot" "${pom_path}" &&
+		grep -q "<artifactId>spring-boot" "${pom_path}"
+	fi
 }
 
 dependencies::has_wildfly_swarm() {
 	local -r app_directory=${1:?}
 	local -r pom_path="${app_directory}/pom.xml"
 
-	[[ -f "${pom_path}" ]] && [[ -n "$(grep "<groupId>org.wildfly.swarm" "${pom_path}")" ]]
+	if [[ -f "${pom_path}" ]]; then
+		grep -q "<groupId>org.wildfly.swarm" "${pom_path}"
+	fi
 }
 
 dependencies::app_requires_postgres() {
 	local -r app_directory=${1:?}
 	local -r pom_path="${app_directory}/pom.xml"
 
-	[[ -f "${pom_path}" ]] &&
-		{
-			[[ -n "$(grep "<groupId>org.postgresql" "${pom_path}")" ]] ||
-				[[ -n "$(grep "<groupId>postgresql" "${pom_path}")" ]] ||
-				[[ -n "$(grep "<groupId>com.impossibl.pgjdbc-ng" "${pom_path}")" ]]
-		}
+	if [[ -f "${pom_path}" ]]; then
+		grep -q "<groupId>org.postgresql" "${pom_path}" ||
+		grep -q "<groupId>postgresql" "${pom_path}" ||
+		grep -q "<groupId>com.impossibl.pgjdbc-ng" "${pom_path}"
+	fi
 }
