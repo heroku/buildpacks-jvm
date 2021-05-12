1d509d02529ce90f44ed76172aae6de24f968b83
Richard Schneeman <rschneeman@salesforce.com>
2021-05-12 14:36:34 -0500

    Exercise a JVM Salesforce Function in tests
    This commit introduces a new fixture that is a simple salesforce function that takes in a text string and reverses it. 
    
    The added test builds the jvm-function buildpack, builds the app with that buildpack, boots the app via docker, and then exercises the function via a local query.

---
+++ test/specs/java-function/spec_helper.rb
@@ -0,0 +1,29 @@
+require "rspec/core"
+require "rspec/retry"
+
+require "cutlass"
+
+def test_dir
+  Pathname(__dir__).join("../..")
+end
+
+JVM_FUNCTION_BUILDPACK = Cutlass::LocalBuildpack.new(directory: test_dir.join("meta-buildpacks/java-function"))
+Cutlass.config do |config|
+  config.default_buildpack_paths = [JVM_FUNCTION_BUILDPACK]
+  config.default_builder = "heroku/buildpacks:18"
+  config.default_repo_dirs = [test_dir.join("fixtures")]
+end
+
+RSpec.configure do |config|
+  # config.filter_run :focus => true
+
+  config.before(:suite) do
+    Cutlass::CleanTestEnv.record
+  end
+
+  config.after(:suite) do
+    JVM_FUNCTION_BUILDPACK.teardown
+    Cutlass::CleanTestEnv.check
+  end
+end
+
