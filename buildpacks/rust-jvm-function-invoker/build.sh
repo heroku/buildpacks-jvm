3c3e295f901bf38b4762be9da064a3643a7ace47
Terence Lee <hone02@gmail.com>
2021-05-20 01:24:07 -0500

    fix shell linting

--- buildpacks/rust-jvm-function-invoker/build.sh
+++ buildpacks/rust-jvm-function-invoker/build.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 buildpack_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-pushd $buildpack_dir
-cargo make pack --profile $CARGO_MAKE_PROFILE
+pushd "$buildpack_dir"
+cargo make pack --profile "$CARGO_MAKE_PROFILE"
 popd
