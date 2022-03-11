1f668eee21a92842ef8615211c0e0abb012ec7a6
Manuel Fuchs <manuel.fuchs@salesforce.com>
2022-03-11 09:24:39 +0100

    Add build.sh

---
+++ buildpacks/maven/build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+buildpack_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+pushd "${buildpack_dir}"
+
+cargo libcnb package --release
+
+mv target target-cargo
+mkdir target
+cp -r target-cargo/buildpack/release/heroku_maven/* target/
+cp package.toml target/
+
+popd
