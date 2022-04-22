fdf196dbdbc183a40043c425333d5c5496c08eeb
Manuel Fuchs <manuel.fuchs@salesforce.com>
2022-04-22 15:52:29 +0200

    Remove heroku/procfile from Cutlass tests

--- test/specs/maven/process_types_spec.rb
+++ test/specs/maven/process_types_spec.rb
@@ -3,10 +3,7 @@ require_relative "spec_helper"
 describe "Heroku's Maven Cloud Native Buildpack" do
   context "for a Spring Boot app" do
     it "will automatically add a process type for that app" do
-      Cutlass::App.new(
-        "buildpack-java-spring-boot-test",
-        buildpacks: [JVM_BUILDPACK, MAVEN_BUILDPACK], # Note the missing Procfile buildpack in the list of buildpacks
-      ).transaction do |app|
+      Cutlass::App.new("buildpack-java-spring-boot-test").transaction do |app|
         app.pack_build do |pack_result|
           app.start_container(expose_ports: 8080) do |container|
             response = Excon.get("http://localhost:#{container.get_host_port(8080)}/", :idempotent => true, :retry_limit => 5, :retry_interval => 1)
