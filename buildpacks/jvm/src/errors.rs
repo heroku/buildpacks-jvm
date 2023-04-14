90579410cf15228866ac0e7691730a619f88aebd
Manuel Fuchs <manuel.fuchs@salesforce.com>
2023-04-14 15:03:07 +0200

    Add crate for common code between JVM buildpacks

--- buildpacks/jvm/src/errors.rs
+++ buildpacks/jvm/src/errors.rs
@@ -1,10 +1,10 @@
 use crate::{
     NormalizeVersionStringError, OpenJdkBuildpackError, ReadVersionStringError, ValidateSha256Error,
 };
+use buildpacks_jvm_shared::log_please_try_again_error;
 use indoc::formatdoc;
 use libherokubuildpack::download::DownloadError;
 use libherokubuildpack::log::log_error;
-use std::fmt::Debug;
 
 #[allow(clippy::too_many_lines)]
 pub(crate) fn on_error_jvm_buildpack(error: OpenJdkBuildpackError) {
@@ -140,22 +140,4 @@ pub(crate) fn on_error_jvm_buildpack(error: OpenJdkBuildpackError) {
             error,
         ),
     }
-}
-
-fn log_please_try_again_error<H: AsRef<str>, M: AsRef<str>, E: Debug>(
-    header: H,
-    message: M,
-    error: E,
-) {
-    log_error(
-        header,
-        formatdoc! {"
-            {message}
-
-            Please try again. If this error persists, please contact us:
-            https://help.heroku.com/
-
-            Details: {error:?}
-        ", message = message.as_ref(), error = error },
-    );
 }
