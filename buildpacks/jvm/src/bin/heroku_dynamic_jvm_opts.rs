44a9984769584ce480b0139b0feb5e9eaf3b22a9
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-01-19 14:50:50 +0100

    Reduce JVM flags to `-XX:MaxRAMPercentage=80.0` for non Eco, Basic and Standard-*X dynos (#631)

--- buildpacks/jvm/src/bin/heroku_dynamic_jvm_opts.rs
+++ buildpacks/jvm/src/bin/heroku_dynamic_jvm_opts.rs
@@ -27,15 +27,13 @@ fn output_from_env(env: &Env) -> HashMap<ExecDProgramOutputKey, String> {
 
 fn dyno_type_jvm_opts() -> Vec<&'static str> {
     match detect_available_memory() {
-        // Free, Hobby, Standard-1X
+        // Eco, Basic, Standard-1X
         Some(536_870_912) => vec!["-Xmx300m", "-Xss512k", "-XX:CICompilerCount=2"],
         // Standard-2X, Private-S
         Some(1_073_741_824) => vec!["-Xmx671m", "-XX:CICompilerCount=2"],
-        // Performance-M, Private-M
-        Some(2_684_354_560) => vec!["-Xmx2g"],
-        // Performance-L, Private-L
-        Some(15_032_385_536) => vec!["-Xmx12g"],
-        _ => vec![],
+        // Rely on JVM ergonomics for other dyno types, but increase the maximum RAM percentage from 25% to 80%.
+        // This is to ensure max heap configuration is aligned to the existing Heroku JVM buildpacks.
+        _ => vec!["-XX:MaxRAMPercentage=80.0"],
     }
 }
 
@@ -44,7 +42,7 @@ fn detect_available_memory() -> Option<usize> {
         "/sys/fs/cgroup/memory.max",
         "/sys/fs/cgroup/memory/memory.limit_in_bytes",
     ]
-    .iter()
+    .into_iter()
     .find_map(|path| std::fs::read_to_string(path).ok())
     .and_then(|contents| contents.trim().parse().ok())
 }
