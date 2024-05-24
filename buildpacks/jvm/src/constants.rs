13c3f248b1e26907828262d37b5b4dc15f778587
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-05-24 13:21:41 +0200

    Remove Heroku's Java Metrics Agent (#679)

--- buildpacks/jvm/src/constants.rs
+++ buildpacks/jvm/src/constants.rs
@@ -1,5 +1,3 @@
 pub(crate) const JAVA_TOOL_OPTIONS_ENV_VAR_DELIMITER: &str = " ";
 pub(crate) const JAVA_TOOL_OPTIONS_ENV_VAR_NAME: &str = "JAVA_TOOL_OPTIONS";
 pub(crate) const JDK_OVERLAY_DIR_NAME: &str = ".jdk_overlay";
-pub(crate) const SKIP_HEROKU_JVM_METRICS_AGENT_INSTALLATION_ENV_VAR_NAME: &str =
-    "SKIP_HEROKU_JVM_METRICS_AGENT_INSTALLATION";
--- buildpacks/jvm/src/util.rs
+++ buildpacks/jvm/src/util.rs
@@ -1,43 +1,10 @@
-use libcnb::Env;
-use std::ffi::OsStr;
 use std::fs::DirEntry;
 use std::path::{Path, PathBuf};
-
-#[derive(Debug)]
-pub(crate) enum ValidateSha256Error {
-    CouldNotObtainSha256(std::io::Error),
-    InvalidChecksum { actual: String, expected: String },
-}
-
-pub(crate) fn validate_sha256<P: AsRef<Path>, S: Into<String>>(
-    path: P,
-    expected_sha256: S,
-) -> Result<(), ValidateSha256Error> {
-    let expected_sha256 = expected_sha256.into();
-
-    libherokubuildpack::digest::sha256(path.as_ref())
-        .map_err(ValidateSha256Error::CouldNotObtainSha256)
-        .and_then(|actual_sha256| {
-            if expected_sha256 == actual_sha256 {
-                Ok(())
-            } else {
-                Err(ValidateSha256Error::InvalidChecksum {
-                    actual: actual_sha256,
-                    expected: expected_sha256,
-                })
-            }
-        })
-}
 
 pub(crate) fn list_directory_contents<P: AsRef<Path>>(path: P) -> std::io::Result<Vec<PathBuf>> {
     std::fs::read_dir(path.as_ref())
         .and_then(Iterator::collect::<std::io::Result<Vec<DirEntry>>>)
         .map(|dir_entries| dir_entries.iter().map(DirEntry::path).collect())
-}
-
-pub(crate) fn boolean_buildpack_config_env_var(env: &Env, key: impl AsRef<OsStr>) -> bool {
-    env.get(key.as_ref())
-        .is_some_and(|value| value != "0" && value != "false" && value != "no")
 }
 
 pub(crate) fn zip_longest<A, B>(a: A, b: B) -> ZipLongest<A, B>
@@ -69,30 +36,5 @@ where
         } else {
             Some((a_item, b_item))
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::util::boolean_buildpack_config_env_var;
-    use libcnb::Env;
-
-    #[test]
-    #[allow(clippy::bool_assert_comparison)]
-    fn test() {
-        let mut env = Env::new();
-        env.insert("FOO", "1");
-        env.insert("BAR", "0");
-        env.insert("BAZ", "false");
-        env.insert("BLAH", "true");
-        env.insert("BLUBB", "yes");
-        env.insert("BLIPP", "no");
-
-        assert_eq!(boolean_buildpack_config_env_var(&env, "FOO"), true);
-        assert_eq!(boolean_buildpack_config_env_var(&env, "BAR"), false);
-        assert_eq!(boolean_buildpack_config_env_var(&env, "BAZ"), false);
-        assert_eq!(boolean_buildpack_config_env_var(&env, "BLAH"), true);
-        assert_eq!(boolean_buildpack_config_env_var(&env, "BLUBB"), true);
-        assert_eq!(boolean_buildpack_config_env_var(&env, "BLIPP"), false);
     }
 }
