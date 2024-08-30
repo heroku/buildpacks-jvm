0d4170ea3e340c91fddacc68f031e5be7c682604
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-08-30 14:01:01 +0200

    Clippy disallow deprecated items again

--- shared/src/env.rs
+++
@@ -1,8 +0,0 @@
-use libcnb::layer::LayerData;
-use libcnb::layer_env::Scope;
-use libcnb::Env;
-
-pub fn extend_build_env<T>(value: LayerData<T>, env: &mut Env) -> LayerData<T> {
-    *env = value.env.apply(Scope::Build, env);
-    value
-}
