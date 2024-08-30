8fcd45003ef49346b2e9f089c13b8220fab58c73
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-08-30 13:03:54 +0200

    Migrate heroku/maven to struct layer API

--- buildpacks/maven/src/layer/maven.rs
+++ buildpacks/maven/src/layer/maven.rs
@@ -1,63 +1,60 @@
 use crate::util::extract_tarball;
 use crate::{MavenBuildpack, MavenBuildpackError, Tarball};
 use libcnb::build::BuildContext;
-use libcnb::data::layer_content_metadata::LayerTypes;
-use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
+use libcnb::data::layer_name;
+use libcnb::layer::{
+    CachedLayerDefinition, InvalidMetadataAction, LayerState, RestoredLayerAction,
+};
 use libcnb::layer_env::{LayerEnv, ModificationBehavior, Scope};
-use libcnb::Buildpack;
+use libcnb::Env;
 use serde::{Deserialize, Serialize};
 use std::fs::File;
-use std::path::Path;
 
-pub(crate) struct MavenLayer {
-    pub(crate) tarball: Tarball,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub(crate) struct MavenLayerMetadata {
-    tarball: Tarball,
-}
-
-impl Layer for MavenLayer {
-    type Buildpack = MavenBuildpack;
-    type Metadata = MavenLayerMetadata;
-
-    fn types(&self) -> LayerTypes {
-        LayerTypes {
-            launch: true,
+pub(crate) fn handle_maven_layer(
+    context: &BuildContext<MavenBuildpack>,
+    tarball: &Tarball,
+    env: &mut Env,
+) -> libcnb::Result<(), MavenBuildpackError> {
+    let layer_ref = context.cached_layer(
+        layer_name!("maven"),
+        CachedLayerDefinition {
             build: true,
-            cache: true,
-        }
-    }
+            launch: true,
+            invalid_metadata_action: &|_| InvalidMetadataAction::DeleteLayer,
+            restored_layer_action: &|metadata: &MavenLayerMetadata, _| {
+                if &metadata.tarball == tarball {
+                    RestoredLayerAction::KeepLayer
+                } else {
+                    RestoredLayerAction::DeleteLayer
+                }
+            },
+        },
+    )?;
 
-    fn create(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        layer_path: &Path,
-    ) -> Result<LayerResult<Self::Metadata>, <Self::Buildpack as Buildpack>::Error> {
+    if let LayerState::Empty { .. } = layer_ref.state {
         let temp_dir = tempfile::tempdir()
             .map_err(MavenBuildpackError::MavenTarballCreateTemporaryDirectoryError)?;
 
         let temp_file_path = temp_dir.path().join("maven.tar.gz");
 
-        libherokubuildpack::download::download_file(&self.tarball.url, &temp_file_path)
+        libherokubuildpack::download::download_file(&tarball.url, &temp_file_path)
             .map_err(MavenBuildpackError::MavenTarballDownloadError)?;
 
         libherokubuildpack::digest::sha256(&temp_file_path)
             .map_err(MavenBuildpackError::MavenTarballSha256IoError)
             .and_then(|downloaded_tarball_sha256| {
-                if downloaded_tarball_sha256 == self.tarball.sha256 {
+                if downloaded_tarball_sha256 == tarball.sha256 {
                     Ok(())
                 } else {
                     Err(MavenBuildpackError::MavenTarballSha256Mismatch {
-                        expected_sha256: self.tarball.sha256.clone(),
+                        expected_sha256: tarball.sha256.clone(),
                         actual_sha256: downloaded_tarball_sha256,
                     })
                 }
             })?;
 
         File::open(&temp_file_path)
-            .and_then(|mut file| extract_tarball(&mut file, layer_path, 1))
+            .and_then(|mut file| extract_tarball(&mut file, &layer_ref.path(), 1))
             .map_err(MavenBuildpackError::MavenTarballDecompressError)?;
 
         // Even though M2_HOME is no longer supported by Maven versions >= 3.5.0, other tooling such
@@ -68,27 +65,21 @@ impl Layer for MavenLayer {
             Scope::Build,
             ModificationBehavior::Override,
             "M2_HOME",
-            layer_path,
+            layer_ref.path(),
         );
 
-        LayerResultBuilder::new(MavenLayerMetadata {
-            tarball: self.tarball.clone(),
-        })
-        .env(layer_env)
-        .build()
+        layer_ref.write_env(layer_env)?;
+        layer_ref.write_metadata(MavenLayerMetadata {
+            tarball: tarball.clone(),
+        })?;
     }
 
-    fn existing_layer_strategy(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        layer_data: &LayerData<Self::Metadata>,
-    ) -> Result<ExistingLayerStrategy, <Self::Buildpack as Buildpack>::Error> {
-        let strategy = if layer_data.content_metadata.metadata.tarball == self.tarball {
-            ExistingLayerStrategy::Keep
-        } else {
-            ExistingLayerStrategy::Recreate
-        };
+    *env = layer_ref.read_env()?.apply(Scope::Build, env);
 
-        Ok(strategy)
-    }
+    Ok(())
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct MavenLayerMetadata {
+    tarball: Tarball,
 }
--- buildpacks/maven/src/layer/maven_repo.rs
+++ buildpacks/maven/src/layer/maven_repo.rs
@@ -1,55 +1,41 @@
-use crate::MavenBuildpack;
+use crate::{MavenBuildpack, MavenBuildpackError};
 use libcnb::build::BuildContext;
-use libcnb::data::layer_content_metadata::LayerTypes;
+use libcnb::data::layer_name;
 use libcnb::generic::GenericMetadata;
-use libcnb::layer::{ExistingLayerStrategy, Layer, LayerData, LayerResult, LayerResultBuilder};
+use libcnb::layer::{CachedLayerDefinition, InvalidMetadataAction, RestoredLayerAction};
 use libcnb::layer_env::{LayerEnv, ModificationBehavior, Scope};
-use libcnb::Buildpack;
-use std::path::Path;
+use libcnb::Env;
 
-pub(crate) struct MavenRepositoryLayer;
-
-impl Layer for MavenRepositoryLayer {
-    type Buildpack = MavenBuildpack;
-    type Metadata = GenericMetadata;
-
-    fn types(&self) -> LayerTypes {
-        LayerTypes {
-            build: true,
+pub(crate) fn handle_maven_repository_layer(
+    context: &BuildContext<MavenBuildpack>,
+    env: &mut Env,
+) -> libcnb::Result<(), MavenBuildpackError> {
+    let layer_ref = context.cached_layer(
+        layer_name!("repository"),
+        CachedLayerDefinition {
+            build: false,
             launch: false,
-            cache: true,
-        }
-    }
+            invalid_metadata_action: &|_| InvalidMetadataAction::DeleteLayer,
+            restored_layer_action: &|_: &GenericMetadata, _| RestoredLayerAction::KeepLayer,
+        },
+    )?;
 
-    fn create(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        layer_path: &Path,
-    ) -> Result<LayerResult<Self::Metadata>, <Self::Buildpack as Buildpack>::Error> {
-        LayerResultBuilder::new(GenericMetadata::default())
-            .env(
-                LayerEnv::new()
-                    .chainable_insert(
-                        Scope::Build,
-                        ModificationBehavior::Delimiter,
-                        "MAVEN_OPTS",
-                        " ",
-                    )
-                    .chainable_insert(
-                        Scope::Build,
-                        ModificationBehavior::Append,
-                        "MAVEN_OPTS",
-                        format!("-Dmaven.repo.local={}", &layer_path.to_string_lossy()),
-                    ),
+    layer_ref.write_env(
+        LayerEnv::new()
+            .chainable_insert(
+                Scope::Build,
+                ModificationBehavior::Delimiter,
+                "MAVEN_OPTS",
+                " ",
             )
-            .build()
-    }
+            .chainable_insert(
+                Scope::Build,
+                ModificationBehavior::Append,
+                "MAVEN_OPTS",
+                format!("-Dmaven.repo.local={}", &layer_ref.path().to_string_lossy()),
+            ),
+    )?;
 
-    fn existing_layer_strategy(
-        &mut self,
-        _context: &BuildContext<Self::Buildpack>,
-        _layer_data: &LayerData<Self::Metadata>,
-    ) -> Result<ExistingLayerStrategy, <Self::Buildpack as Buildpack>::Error> {
-        Ok(ExistingLayerStrategy::Keep)
-    }
+    *env = layer_ref.read_env()?.apply(Scope::Build, env);
+    Ok(())
 }
