cef512e9508ff8caad26d0bb6ea7fe45f69716f6
Manuel Fuchs <manuel.fuchs@salesforce.com>
2024-12-19 17:55:28 +0100

    Fix JDK overlay directory

--- buildpacks/jvm/CHANGELOG.md
+++ buildpacks/jvm/CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+### Fixed
+
+- JDK overlays are now properly sourced from the `.jdk-overlay` directory instead of `.jdk_overlay`. ([#763](https://github.com/heroku/buildpacks-jvm/pull/763))
+
 ## [6.0.4] - 2024-12-05
 
 ### Added
---
+++ buildpacks/jvm/tests/integration/overlay.rs
@@ -0,0 +1,42 @@
+use crate::default_build_config;
+use libcnb_test::{assert_contains, TestRunner};
+use std::path::PathBuf;
+
+#[test]
+#[ignore = "integration test"]
+fn overlay() {
+    const ADDITIONAL_PATH: &str = "earth.txt";
+    const ADDITIONAL_CONTENTS: &str = "Un diminuto punto azul";
+
+    const CACERTS_PATH: &str = "lib/security/cacerts";
+    const CACERTS_CONTENTS: &str = "overwritten";
+
+    TestRunner::default().build(
+        default_build_config("test-apps/java-21-app").app_dir_preprocessor(|app_dir| {
+            let overlay_path = app_dir.join(".jdk-overlay");
+            std::fs::create_dir_all(&overlay_path).unwrap();
+            std::fs::write(overlay_path.join(ADDITIONAL_PATH), ADDITIONAL_CONTENTS).unwrap();
+
+            let cacerts_path = overlay_path.join(PathBuf::from(CACERTS_PATH));
+            std::fs::create_dir_all(cacerts_path.parent().unwrap()).unwrap();
+            std::fs::write(&cacerts_path, CACERTS_CONTENTS).unwrap();
+        }),
+        |context| {
+            // Validate that adding a new file works
+            assert_contains!(
+                context
+                    .run_shell_command(format!("cat $JAVA_HOME/{ADDITIONAL_PATH}"))
+                    .stdout,
+                ADDITIONAL_CONTENTS
+            );
+
+            // Validate that overwriting a file works
+            assert_contains!(
+                context
+                    .run_shell_command(format!("cat $JAVA_HOME/{CACERTS_PATH}"))
+                    .stdout,
+                CACERTS_CONTENTS
+            );
+        },
+    );
+}
