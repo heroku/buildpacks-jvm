7be037753a7035e3ccfbb104841d4d2265c4365d
Schneems <richard.schneeman+foo@gmail.com>
2025-01-22 16:32:23 -0600

    Fix tests

--- buildpacks/maven/tests/integration/settings_xml.rs
+++ buildpacks/maven/tests/integration/settings_xml.rs
@@ -31,7 +31,7 @@ fn maven_settings_url_failure() {
             |context| {
                 assert_contains!(
                     context.pack_stderr,
-                    &format!("You have set MAVEN_SETTINGS_URL to \"{SETTINGS_XML_URL_404}\". We tried to download the file at this\nURL, but the download failed. Please verify that the given URL is correct and try again.")
+                    &format!("You have set MAVEN_SETTINGS_URL to \"{SETTINGS_XML_URL_404}\". We tried to download the file at this")
                 );
 
                 // This error message comes from Maven itself. We don't expect Maven to to be executed at all.
